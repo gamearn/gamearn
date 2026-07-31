c68abc7df27ff8c837aae568cfe81f8069055c7b
Greatman Victor <godpikin2009@gmail.com>
2026-07-31 16:22:40 +0100

    chore: rename Android package to com.gamearn, drop stray analyze.txt
    - namespace + applicationId: com.gamearn.gaming_platform -> com.gamearn
    - MainActivity.kt moved to com/gamearn/
    - iOS bundle id: com.gamearn.gamingPlatform -> com.gamearn
    - remove stale analyze.txt dump

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.gamearn.gaming_platform"
+    namespace = "com.gamearn"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -43,7 +43,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.gamearn.gaming_platform"
+        applicationId = "com.gamearn"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/gamearn/MainActivity.kt
+++ android/app/src/main/kotlin/com/gamearn/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.gamearn.gaming_platform
+package com.gamearn
 
 import io.flutter.embedding.android.FlutterActivity
 
