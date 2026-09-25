ef45d93f44a0ede7f67efebf000301274135db70
Olayimka <111387535+Olayimka@users.noreply.github.com>
2026-09-25 23:22:59 +0100

    Revert "chore: remove Firebase plugins and configuration"
    This reverts commit 6294e44526037f02492f094ea123e7800312287d.

---
+++ plugins/withFixFirebaseIOS.js
@@ -0,0 +1,44 @@
+const { withDangerousMod } = require('expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function withFixFirebaseIOS(config) {
+  return withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
+      if (fs.existsSync(podfilePath)) {
+        let contents = fs.readFileSync(podfilePath, 'utf8');
+
+        // 1. Inject $RNFirebaseDisableSPM = true and use_modular_headers! at the top
+        if (!contents.includes('$RNFirebaseDisableSPM')) {
+          contents = `$RNFirebaseDisableSPM = true\n` + contents;
+        }
+        if (!contents.includes('use_modular_headers!')) {
+          contents = `use_modular_headers!\n` + contents;
+        }
+
+        // 2. Inject post_install settings for non-modular includes & iOS 15.1 target
+        const postInstallSnippet = `
+    installer.pods_project.targets.each do |target|
+      target.build_configurations.each do |config|
+        config.build_settings['CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'] = 'YES'
+        config.build_settings['IPHONEOS_DEPLOYMENT_TARGET'] = '15.1'
+      end
+    end
+`;
+        if (contents.includes('post_install do |installer|')) {
+          if (!contents.includes('CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES')) {
+            contents = contents.replace(
+              'post_install do |installer|',
+              `post_install do |installer|${postInstallSnippet}`
+            );
+          }
+        }
+
+        fs.writeFileSync(podfilePath, contents, 'utf8');
+      }
+      return config;
+    },
+  ]);
+};
